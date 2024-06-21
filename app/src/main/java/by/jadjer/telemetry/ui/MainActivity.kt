eba3178abda1d8c641e604f9b76fdc223b4e104e
jadjer <jadjer@ya.ru>
2024-06-21 19:08:36 +0300

    Add LICENSE file

--- app/src/main/java/by/jadjer/telemetry/ui/MainActivity.kt
+++ app/src/main/java/by/jadjer/telemetry/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,11 +15,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import by.jadjer.telemetry.ui.theme.MotoGymkhanasTelemetryAssistantTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: TelemetryHomeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MotoGymkhanasTelemetryAssistantTheme {
+                val windowSize = calculateWindowSizeClass(this)
+                val displayFeatures = calculateDisplayFeatures(this)
+                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -27,21 +35,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MotoGymkhanasTelemetryAssistantTheme {
-        Greeting("Android")
     }
 }
